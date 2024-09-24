97d10a84b19ebccb4650f7f55f3575814b37c590
Arnaud Taffanel <arnaud@bitcraze.io>
2024-09-24 13:56:31 +0200

    Change release file name to match motors name

--- bitcraze_tools/build_bl_fw.sh
+++ bitcraze_tools/build_bl_fw.sh
@@ -14,14 +14,14 @@ sed -i 's/^DEFAULT_PGM_DIRECTION EQU ./DEFAULT_PGM_DIRECTION EQU 1/' src/Setting
 touch src/Settings/BluejaySettings.asm
 
 make clean single_target
-cp build/hex/O_H_10_48_v0.21.1-RC1.hex $run_dir/cfbl2.1_esc_cw.hex
+cp build/hex/O_H_10_48_v0.21.1-RC1.hex $run_dir/cfbl2.1_esc_normal_m1-m3.hex
 
 # Change the default direction to counter-clockwise
 sed -i 's/^DEFAULT_PGM_DIRECTION EQU ./DEFAULT_PGM_DIRECTION EQU 2/' src/Settings/BluejaySettings.asm
 touch src/Settings/BluejaySettings.asm
 
 make clean single_target
-cp build/hex/O_H_10_48_v0.21.1-RC1.hex $run_dir/cfbl2.1_esc_ccw.hex
+cp build/hex/O_H_10_48_v0.21.1-RC1.hex $run_dir/cfbl2.1_esc_reverse_m2-m4.hex
 
 # Restore the original file
 git checkout src/Settings/BluejaySettings.asm
